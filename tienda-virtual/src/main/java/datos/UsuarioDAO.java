c80d87531d964636a7156628a53b514b64883ae8
Sandra Penaranda <sandraelizabethpenaranda@gmail.com>
2019-12-08 11:14:03 -0500

    creación de clase UsuarioDAO

--- tienda-virtual/src/main/java/datos/UsuarioDAO.java
+++ tienda-virtual/src/main/java/datos/UsuarioDAO.java
@@ -1,26 +1,29 @@
 package datos;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import modelo.Usuario;
 
 public class UsuarioDAO {
 
+	@Inject
 	private EntityManager em;
 	
 	public void insertar(Usuario usuario) {
-		
+		em.persist(usuario);
 	}
 	
 	public void actualizar(Usuario usuario) {
-		
+		em.merge(usuario);
 	}
 	
 	public void borrar(String cedula) {
-		
+		em.remove(leer(cedula));
 	}
 	
 	public Usuario leer(String cedula) {
+		em.find(Usuario.class, cedula);
 		return null;
 	}
 }
