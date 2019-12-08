48e0c9917da1a0cd3f830f8860a100b56ef80784
jmaguana <53019106+jmaguana@users.noreply.github.com>
2019-12-08 10:13:32 -0500

    Creación de proyecto.

---
+++ tienda-virtual/src/main/java/modelo/Usuario.java
@@ -0,0 +1,73 @@
+package modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Usuario {
+
+
+	@Id
+	@NotEmpty
+	private String cedula;
+	
+	@NotNull
+	@Size(min=4, max=20)
+	private String nombre;
+	
+	@NotNull
+	@Size(min=4, max=20)
+	private String apellidos;
+	
+	@Email
+	private String email;
+	
+	private String pass;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
