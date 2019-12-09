c6bac93fe8c9290225e9eb3783f12a324380e619
jmaguana <53019106+jmaguana@users.noreply.github.com>
2019-12-09 16:24:27 -0500

    Crud Producto

--- tienda-virtual/src/main/java/vista/ProductoBean.java
+++ tienda-virtual/src/main/java/vista/ProductoBean.java
@@ -1,5 +1,55 @@
 package vista;
 
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import datos.ProductoDAO;
+import modelo.Producto;
+
+@ManagedBean
 public class ProductoBean {
 
+	private Producto producto;
+	private List<Producto> lista;
+	private int codigo;
+	
+	@Inject
+	private ProductoDAO pDao;
+
+	public void init() {
+		producto = new Producto();
+		loadProducto();
+	}
+	
+	public void loadProducto() {
+		lista = pDao.listar();
+	}
+	
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public List<Producto> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Producto> lista) {
+		this.lista = lista;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	
 }
